190e8f02203fc3f7a0e6c30b5cb2edb9492bb522
AriReiva <rrardi0202@gmail.com>
2025-11-23 12:01:54 +0700

    user

--- routes/router.go
+++ routes/router.go
@@ -47,6 +47,7 @@ func UserRouter(r *gin.RouterGroup) {
 		transactionGroup.GET("/:id", controllers.GetUserById)
 		transactionGroup.POST("/", controllers.CreateUser)
 		transactionGroup.PATCH("/:id/payment", controllers.UpdateUser)
+		transactionGroup.PUT("/:id", controllers.UpdateUser)
 		transactionGroup.DELETE("/:id", controllers.DeleteUser)
 	}
 }
